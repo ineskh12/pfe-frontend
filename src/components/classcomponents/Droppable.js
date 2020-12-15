5ec4a8fc63502ebcc720c6c9ed43cf784f565873
ines khelifi <ines.khelifi.1@esprit.tn>
2020-12-15 22:32:13 +0100

    editor+login+register

--- src/components/classcomponents/Droppable.js
+++ src/components/classcomponents/Droppable.js
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 import { Editor } from "react-draft-wysiwyg";
 import { ContentState, EditorState, convertFromRaw } from "draft-js";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
-import { stateToHTML } from "draft-js-export-html";
 import "./toolbar.css"
 
 import FormatBoldIcon from '@material-ui/icons/FormatBold';
